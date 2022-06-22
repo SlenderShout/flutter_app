3a961466657b0ffc34f67f4451d63a05d785ea74
Murat Can <44754026+SlenderShout@users.noreply.github.com>
2022-06-22 13:25:02 +0300

    New build
    old build name changed to main2.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,204 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(
-      /*MaterialApp(
-        home: Scaffold(
-          backgroundColor: Color(0xFFD7E0FF),
-          appBar: AppBar(
-            backgroundColor: Colors.lime[900],
-            title: const Center(
-              child: Text('Merhaba Dünya'),
-            ),
-          ),
-          body: Center(
-            child: Column(
-              children: [
-                Center(
-                  child: Image.asset('Images/mutlu_bayramlar.jpg'),
-                ),
-                const Center(
-                  child: Text(
-                    'Mutlu Bayramlar',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),*/
-      const MyApp(),
-    );
+void main() => runApp(MyApp2());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        /*appBar: AppBar(
-          backgroundColor: Colors.black,
+        appBar: AppBar(
           centerTitle: true,
-          title: Text('Selam Dünya'),
-        ),*/
-        backgroundColor: Colors.brown[500], //const Color(0xff616161),
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const CircleAvatar(
-                  radius: 70,
-                  backgroundImage: AssetImage('assets/images/kahve.jpg'),
-                ),
-                const Text(
-                  "Bi' Kahve versem ?",
-                  style: TextStyle(
-                    fontFamily: 'DancingScript',
-                    fontSize: 37,
-                    color: Color(0xffd7ccc8),
-                  ),
-                ),
-                Text(
-                  'Sadece bir tık uzağında',
-                  style: GoogleFonts.lobster(
-                    fontSize: 18,
-                    color: Color(0xffefebe9),
-                  ),
-                ),
-                /*TextStyle(
-                    fontFamily: 'DancingScript',
-                    fontSize: 18,
-                    color: Color(0xffefebe9),
-                  ),*/
-              ],
+          title: Text('Bugünün Spesiyali'),
+        ),
+        body: nimetler(),
+      ),
+    );
+  }
+}
+
+class nimetler extends StatefulWidget {
+  const nimetler({Key? key}) : super(key: key);
+
+  @override
+  State<nimetler> createState() => _nimetlerState();
+}
+
+class _nimetlerState extends State<nimetler> {
+  int i = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {},
+                  child: Image.asset('assets/images/corba_$i.jpg')),
             ),
           ),
-        ),
-        /*Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                color: Colors.white,
-                height: 100,
-                child: Text('1. Containercik'),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.yellow,
-                    width: 100,
-                    height: 100,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.lightBlue,
-                width: 200,
-                height: 100,
-                child: Text('2. Containercik'),
-              )
-            ],
-          ),*/
-        /*Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Flexible(
-                child: Container(
-                  color: Colors.orange,
-                  width: 200,
-                  height: 100,
-                  child: const Text('1. Container'),
-                ),
-              ),
-              const SizedBox(
-                width: 30,
-              ),
-              Flexible(
-                child: Container(
-                  color: Colors.green,
-                  width: 200,
-                  height: 100,
-                  child: const Text('2. Container'),
-                ),
-              ),
-              const SizedBox(
-                width: 30,
-              ),
-              Flexible(
-                child: Container(
-                  color: Colors.blue,
-                  width: 200,
-                  height: 100,
-                  child: const Text('3. Container'),
-                ),
-              ),
-            ],
-          ),*/ // Row Widgeti
-        /*Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                color: Colors.orange,
-                width: 200,
-                height: 100,
-                child: Text('1. Container'),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                color: Colors.green,
-                width: 200,
-                height: 100,
-                child: Text('2. Container'),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                color: Colors.blue,
-                width: 200,
-                height: 100,
-                child: Text('3. Container'),
-              ),
-            ],
-          ),*/ //Column Widgeti
-        /*Container(
-            //transform: Matrix4.rotationZ(-0.2),
-            width: 100,
-            height: 100,
-            alignment: Alignment.topRight,
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadiusDirectional.circular(16),
-              color: Colors.white,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  onPressed: () {},
+                  child: Image.asset('assets/images/yemek_$i.jpg')),
             ),
-            //margin: const EdgeInsets.all(16), //Container Boşluk
-            margin: const EdgeInsets.only(
-              left: 12,
-              top: 12,
-            ),
-            padding: const EdgeInsets.all(16), //Elemanın Boşluğu
-            child: Center(
-              child: Transform(
-                transform: Matrix4.rotationZ(-0.2),
-                alignment: FractionalOffset.center,
-                child: Text('Deneme'),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {},
+                child: Image.asset('assets/images/tatli_$i.jpg'),
               ),
             ),
-          ),*/ // Tekli Container
+          ),
+        ],
       ),
     );
   }
